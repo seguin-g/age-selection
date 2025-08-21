28646d832c3aab4c87b0b1a31812c58e8886bf59
Guillaume Seguin <seguing@google.com>
2025-08-21 17:04:57 -0400

    Fichiers initiaux du projet MakeCode

---
+++ main.py
@@ -0,0 +1,25 @@
+def on_button_pressed_a():
+    global age
+    age += max(0, -1)
+    display_age(age)
+
+def on_button_pressed_b():
+    global age
+    age = min(age+1, 99)
+    display_age(age)
+
+def display_age(age):
+    basic.clear_screen()
+    # Decenies
+    for i in range(0, (age/10)):
+        led.plot(i%5, i/5)
+    # Annees
+    for i in range(0, (age%10)):
+        led.plot(i%5, i/5+3)
+
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+age = 60
+display_age(age)
---
+++ main.ts
@@ -0,0 +1,25 @@
+function display_age(age: number) {
+    let i: number;
+    basic.clearScreen()
+    //  Decenies
+    for (i = 0; i < age / 10; i++) {
+        led.plot(i % 5, i / 5)
+    }
+    //  Annees
+    for (i = 0; i < age % 10; i++) {
+        led.plot(i % 5, i / 5 + 3)
+    }
+}
+
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    age += Math.max(0, -1)
+    display_age(age)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    age = Math.min(age + 1, 99)
+    display_age(age)
+})
+let age = 60
+display_age(age)
